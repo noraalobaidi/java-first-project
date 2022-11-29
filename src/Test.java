0ba5557d8a4fe4a15e95f1a8e98f48e90fb93d43
Nora Khalid <nalobaidi_@hotmail.com>
2022-11-29 19:08:28 +0300

    MODIFIED deletefile method

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Test {
@@ -10,15 +11,16 @@ public class Test {
 	public static void main(String[] args) {
 		Test user = new Test();
 //		user.createFile();
-//		user.sortByName();
-		user.searchFile();
+		user.sortByName();
+//		user.searchFile();
+//		user.deleteFile();
 	}
 	
 	public void createFile() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter file name :");
 		String filename = input.next();
-		 File theFile = new File("../java-first-project/files/"+filename+".txt"); //This wouldn't create a file
+		 File theFile = new File("../java-first-project/files/"+filename); //This wouldn't create a file
 	        System.out.println();
 	        if (theFile.exists())
 	        {
@@ -55,7 +57,13 @@ public class Test {
 		for(String file:arr) {array.add(file);}
 
 		for(String file :array) {System.out.println(file);}
-		Collections.sort(array);
+//		Collections.sort(array);
+		Collections.sort(array, new Comparator<String>() {
+		    @Override
+		    public int compare(String s1, String s2) {
+		        return s1.compareToIgnoreCase(s2);
+		    }
+		});
 		System.out.println("list sorted");
 		for(String file :array) {System.out.println(file);}
 		 
@@ -66,12 +74,12 @@ public class Test {
 		
 		String [] arrOfAllFiles=getAllFiles();
 		ArrayList<String> array = new ArrayList();
-		for(String file:arrOfAllFiles) {array.add(file);}
+		for(String f:arrOfAllFiles) {array.add(f);}
 		
 		System.out.print("Enter the name of the file:");
 		String searchfile= input.nextLine();
 		
-		for (String f:arrOfAllFiles) {
+		for (String f:array) {
 			
 			if (searchfile.toLowerCase().equals(f.toLowerCase()))
 			{
@@ -82,6 +90,39 @@ public class Test {
 		System.out.println("File "+searchfile+" not found");
 		
 		
+		
+	}
+	
+	public void deleteFile () {
+		
+Scanner input = new Scanner(System.in);
+		
+		String [] arrOfAllFiles=getAllFiles();
+		ArrayList<String> array = new ArrayList();
+		for(String f:arrOfAllFiles) {array.add(f);}
+		
+		System.out.print("Enter the name of the file you want to delete:");
+		String searchfile= input.nextLine();
+		
+		for (String f:array) {
+			
+			if (searchfile.toLowerCase().equals(f.toLowerCase()))
+			{
+File fileToDelete= new File("../java-first-project/files/"+f);
+		if (fileToDelete.delete())
+		{
+			System.out.println("File "+f+" was deleted successfully");
+			}
+		else 
+		{
+			System.out.println("Failed to delete the file ");  
+  
+		}
+		
+return;
+			}
+		}
+		System.out.println("File "+searchfile+" not found");
 		
 	}
 	
