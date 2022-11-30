deed9d535b864ea14dc1ba960059c0c66caf9ef1
Nora Khalid <nalobaidi_@hotmail.com>
2022-11-30 22:10:13 +0300

    FINALIZED the code

--- src/LockersPvt.java
+++ src/LockersPvt.java
@@ -5,32 +5,43 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class Test {
+public class LockersPvt {
 
 	
 	public static void main(String[] args) {
-		Test user = new Test();
-//		user.createFile();
-		user.sortByName();
-//		user.searchFile();
-//		user.deleteFile();
+		LockersPvt user = new LockersPvt();
+		MainMenu menu = new MainMenu();
+		int opt;
+		
+		do {
+			menu.displayMenu();
+			opt = menu.getOpt();
+			menu.switchOpt(opt);
+			}
+		while(opt!=3);
+//		System.out.println("end");
+		
+
+		
+		
+
 	}
 	
 	public void createFile() {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter file name :");
+		System.out.print("    Enter file name :");
 		String filename = input.next();
 		 File theFile = new File("../java-first-project/files/"+filename); //This wouldn't create a file
 	        System.out.println();
 	        if (theFile.exists())
 	        {
-	        	System.out.println("The file "+theFile.getName()+" already exists");
+	        	System.out.println("    The file "+theFile.getName()+" already exists");
 	        }
 	        else 
 	        {
 	        	try {
 		            theFile.createNewFile();//Actually creates it
-		            System.out.println("The file "+theFile.getName()+" created Successfully !");
+		            System.out.println("    The file "+theFile.getName()+" was created Successfully !");
 		        } catch (IOException e) {
 		            throw new RuntimeException(e);
 		        }
@@ -56,16 +67,16 @@ public class Test {
 
 		for(String file:arr) {array.add(file);}
 
-		for(String file :array) {System.out.println(file);}
-//		Collections.sort(array);
 		Collections.sort(array, new Comparator<String>() {
 		    @Override
 		    public int compare(String s1, String s2) {
 		        return s1.compareToIgnoreCase(s2);
 		    }
 		});
-		System.out.println("list sorted");
-		for(String file :array) {System.out.println(file);}
+		System.out.println("    Files list sorted in ascending order:");
+		System.out.println("    ****************************************");
+		for(String file :array) {System.out.println("  "+file);}
+		
 		 
 	}
 	
@@ -76,18 +87,21 @@ public class Test {
 		ArrayList<String> array = new ArrayList();
 		for(String f:arrOfAllFiles) {array.add(f);}
 		
-		System.out.print("Enter the name of the file:");
+		System.out.print("    Enter the name of the file:");
 		String searchfile= input.nextLine();
+		System.out.println("    ****************************");
 		
 		for (String f:array) {
 			
 			if (searchfile.toLowerCase().equals(f.toLowerCase()))
 			{
-				System.out.println("File "+f+" is found");
+				System.out.println("   File "+f+" was found");
+//				System.out.println("  *********************");
 				return;
 			}
 		}
-		System.out.println("File "+searchfile+" not found");
+		System.out.println("    File "+searchfile+" was not found");
+//		System.out.println("  ***********************************");
 		
 		
 		
@@ -95,34 +109,35 @@ public class Test {
 	
 	public void deleteFile () {
 		
-Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		
 		String [] arrOfAllFiles=getAllFiles();
 		ArrayList<String> array = new ArrayList();
 		for(String f:arrOfAllFiles) {array.add(f);}
 		
-		System.out.print("Enter the name of the file you want to delete:");
+		System.out.print("    Enter the name of the file you want to delete:");
 		String searchfile= input.nextLine();
+		System.out.println("    **********************************************");
+		
 		
 		for (String f:array) {
 			
 			if (searchfile.toLowerCase().equals(f.toLowerCase()))
 			{
-File fileToDelete= new File("../java-first-project/files/"+f);
-		if (fileToDelete.delete())
-		{
-			System.out.println("File "+f+" was deleted successfully");
+				File fileToDelete= new File("../java-first-project/files/"+f);
+				if (fileToDelete.delete())
+				{
+					System.out.println("    File "+f+" was deleted successfully");
+					}
+				else 
+				{
+					System.out.println("    Failed to delete the file ");  
+		  
+				}
+				return;
 			}
-		else 
-		{
-			System.out.println("Failed to delete the file ");  
-  
 		}
-		
-return;
-			}
-		}
-		System.out.println("File "+searchfile+" not found");
+		System.out.println("    File "+searchfile+" was not found");
 		
 	}
 	
--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,127 @@
+import java.util.Scanner;
 
-public class Menu {
+public abstract class Menu {
+ public abstract void displayMenu();
 
+public int getOpt() {
+	 Scanner input = new Scanner(System.in);
+	 int opt = input.nextInt();
+	 return opt;
+ }
+ 
+ public abstract void switchOpt(int opt);
+ 
+	
 }
+
+ class MainMenu extends Menu{
+
+	
+	 @Override
+		public void displayMenu() {
+			System.out.println("\n  *************************************************");
+			System.out.println("  *                  Main Menu                    *");
+			System.out.println("  *************************************************");
+			System.out.println("  * Please select an option from the list:        *");
+			System.out.println("  *                                               *");
+			System.out.println("  *       1- Sort files in ascending order        *");
+			System.out.println("  *       2- Files Sub-menu                       *");
+			System.out.println("  *       3- Exit                                 *");
+			System.out.println("  *                                               *");
+			System.out.println("  *************************************************");
+			System.out.print("    Enter selection and then press ENTER :");
+			
+			
+			
+			
+			
+		}
+
+	@Override
+	public void switchOpt(int opt) {
+		
+//		SubMenu sub = new SubMenu();
+		switch (opt){
+		
+		case 1:
+			System.out.println("\n");
+			new LockersPvt().sortByName();
+			break;
+		case 2:
+			SubMenu sub = new SubMenu();
+			int option;
+			do {
+				sub.displayMenu();
+				option=sub.getOpt();
+				sub.switchOpt(option);
+			}
+			while(option!=4);
+			break;
+		case 3:
+			System.exit(0);
+			break;
+		default:
+			System.out.println("");
+			System.out.print("\n\n    --- Please enter a value between 1 to 3 ---\n\n");
+
+		}
+		
+	}
+	 
+}
+ 
+ class SubMenu extends Menu{
+
+	 @Override
+		public void displayMenu() {
+			System.out.println("\n  *************************************************");
+			System.out.println("  *                Files sub-menu                 *");
+			System.out.println("  *************************************************");
+			System.out.println("  * Please select an option from the list:        *");
+			System.out.println("  *                                               *");
+			System.out.println("  *       1- Add file                             *");
+			System.out.println("  *       2- Delete file                          *");
+			System.out.println("  *       3- Search for a file                    *");
+			System.out.println("  *       4- Back                                 *");
+			System.out.println("  *                                               *");
+			System.out.println("  *************************************************");
+			System.out.print("    Enter selection and then press ENTER :");
+			
+			
+			
+			
+			
+		}
+
+	@Override
+	public void switchOpt(int opt) {
+		
+		switch (opt){
+		
+		case 1:
+			System.out.println("\n");
+			new LockersPvt().createFile();
+			break;
+		case 2:
+			System.out.println("\n");
+			new LockersPvt().deleteFile();
+			break;
+		case 3:
+			System.out.println("\n");
+			new LockersPvt().searchFile();
+			break;
+		case 4:
+//			System.out.println("\n");
+//			new MainMenu().displayMenu();
+			break;
+		default:
+			System.out.println("");
+			System.out.print("\n\n    --- Please enter a value between 1 to 4 ---\n\n");
+
+		}
+		
+	}
+	 
+	 
+ }
+ 
